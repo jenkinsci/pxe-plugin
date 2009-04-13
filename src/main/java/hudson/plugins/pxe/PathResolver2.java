728f96711eba34bff9e8e1269579d616677f1a63
kohsuke <kohsuke@dev.java.net>
2009-04-13 19:07:39 +0000

    doc improvement

--- src/main/java/hudson/plugins/pxe/PathResolver2.java
+++ src/main/java/hudson/plugins/pxe/PathResolver2.java
@@ -1,6 +1,7 @@
 package hudson.plugins.pxe;
 
 import org.jvnet.hudson.tftpd.PathResolver;
+import org.jvnet.hudson.tftpd.Data;
 
 import java.io.InputStream;
 import java.io.IOException;
@@ -11,6 +12,12 @@ import java.io.IOException;
  * @author Kohsuke Kawaguchi
  */
 interface PathResolver2 extends PathResolver {
+    /**
+     * The same as {@link Data#size()}.
+     */
     int size(String path) throws IOException;
+    /**
+     * The same as {@link Data#read()}.
+     */
     InputStream read(String path) throws IOException;
 }
