6a43662ba9e1616220787f7b5c44b044d43768bf
kohsuke <kohsuke@dev.java.net>
2009-04-14 00:12:31 +0000

    adding pxelinux.cfg/default merge support

--- src/main/java/hudson/plugins/pxe/BootConfiguration.java
+++ src/main/java/hudson/plugins/pxe/BootConfiguration.java
@@ -5,6 +5,8 @@ import hudson.model.Hudson;
 import hudson.DescriptorExtensionList;
 import org.jvnet.hudson.tftpd.Data;
 
+import java.io.IOException;
+
 /**
  * @author Kohsuke Kawaguchi
  */
@@ -13,7 +15,33 @@ public abstract class BootConfiguration implements Describable<BootConfiguration
         return (BootConfigurationDescriptor) Hudson.getInstance().getDescriptor(getClass());
     }
 
-    public abstract Data tftp(String fileName);
+    /**
+     * Serves data from TFTP.
+     *
+     * <p>
+     * This mechanism is useful when you need to generate the data to be served on the fly.
+     * Static resources can be more easily served by simply placing them as resources
+     * under /tftp.
+     *
+     * <p>
+     * The TFTP file namespace is a shared resources among all {@link BootConfiguration}s,
+     * so plugins are expected to use some prefix to avoid collisions.
+     *
+     * @return
+     *      null if no such file exists, as far as this plugin is concerned.
+     *      The PXE plugin will continue to search other {@link BootConfiguration}s to
+     *      see if anyone understands it.
+     * @throws IOException
+     *      If a problem occurs. The PXE plugin will abort the search and the download will fail.
+     */
+    public Data tftp(String fileName) throws IOException {
+        return null;
+    }
+
+    /**
+     * Returns the fragment to be merged into <tt>pxelinux.cfg/default</tt>.
+     */
+    public abstract String getPxeLinuxConfigFragment() throws IOException;
 
     /**
      * Returns all the registered {@link BootConfigurationDescriptor}s.
--- src/main/java/hudson/plugins/pxe/PathResolverImpl.java
+++ src/main/java/hudson/plugins/pxe/PathResolverImpl.java
@@ -3,6 +3,8 @@ package hudson.plugins.pxe;
 import hudson.model.Hudson;
 import hudson.remoting.Channel;
 import hudson.remoting.RemoteInputStream;
+import hudson.util.DescribableList;
+import org.apache.commons.io.IOUtils;
 import org.jvnet.hudson.tftpd.Data;
 import org.jvnet.hudson.tftpd.PathResolver;
 
@@ -20,21 +22,30 @@ import java.net.URL;
  * @author Kohsuke Kawaguchi
  */
 final class PathResolverImpl implements PathResolver2, Serializable {
-    public Data open(String fileName) throws IOException {
+    public Data open(final String fileName) throws IOException {
         // static resource first
         URL res = Hudson.getInstance().getPluginManager().uberClassLoader.getResource("tftp/"+fileName);
         if(res!=null)
-            return Data.fromURL(res);
+            return Data.from(res);
 
         // dynamic resources
-        for(BootConfiguration config : Hudson.getInstance().getPlugin(PluginImpl.class).getBootConfigurations()) {
+        DescribableList<BootConfiguration,BootConfigurationDescriptor> bootConfigurations = Hudson.getInstance().getPlugin(PluginImpl.class).getBootConfigurations();
+
+        for(BootConfiguration config : bootConfigurations) {
             Data d = config.tftp(fileName);
             if(d!=null)     return d;
         }
 
-        // TODO: combined fragments --- pxelinux.0/default
+        if(fileName.equals("pxelinux.cfg/default")) {
+            // combine all pxelinux.cfg.fragment files into one and serve them
+            StringBuilder buf = new StringBuilder(IOUtils.toString(getClass().getClassLoader().getResourceAsStream("tftp/"+fileName)));
+            // merge all fragments
+            for (BootConfiguration conf : bootConfigurations)
+                buf.append(conf.getPxeLinuxConfigFragment()).append('\n');
+            return Data.from(buf.toString());
+        }
 
-        return null;
+        throw new IOException("No such file: "+fileName);
     }
 
     public int size(String path) throws IOException {
